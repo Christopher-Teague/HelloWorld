e731bc15db9ec73096c6be17263951aa901a2875
Christopher-Teague <cmteague77@gmail.com>
2021-10-25 14:44:18 -0700

    final

--- hello_world.py
+++ hello_world.py
@@ -7,7 +7,7 @@ print("hello "+ name )	# with a +
 # 3. print "Hello 42!" with the number in a variable
 name = 42
 print( "hello", name, "!" )	# with a comma
-print("hello " + str(name))	# with a +	-- this one should give us an error!
+print("hello " + str(name) + "!")	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
